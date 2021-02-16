bacef99fa6a15ee79b00f921a15d77da2cdf62d6
Erik Kihn <erikkihn97@gmail.com>
2021-02-16 11:23:38 +0200

    muutoksia

--- arvostelut2.php
+++ arvostelut2.php
@@ -1,62 +1,52 @@
  <?php
 
-// Tarkistaa onko submit nappi painettu eli onko post metodia submit
-if (isset($_POST['submit'])) {
-   $nickanme = (isset($_POST['nickname'])); // undefined index
-   $kaupunki = $_POST['kaupunki']; // undefined index
-   $textarea = (isset($_POST['textarea']));
-   $rating = (isset($_POST['rating'])); // undefined index
+$message="Toimii";
 
-   // Deafaultisti on false
-   $errorEmpty = false;
-   
-   // Tarkistetaan onko tyhjiä kenttiä formissa
-   if (empty($nickanme)) { // ei toimi
-       // class form-error on new.css filessä
-        echo "<span class='form-error'>Fill in a nickname!</span>";
-        $errorEmpty = true;
-   }
-   // if (empty($kaupunki)) {
-   //   echo "<span class='form-error'>Choose a city!</span>";
-   //   $errorEmpty = true;
-   if (empty($textarea)) {
-      echo "<span class='form-error'>Fill in the textarea!</span>";
-      $errorEmpty = true;
-   }
-   if (empty($rating)) {
-      echo "<span class='form-error'>Choose a rating!</span>";
-      $errorEmpty = true;
-   }
-   else{ // Jos ei tule virheitä niin tulee onnistumis viesti
-       // class form-success on new.css filessä
-       echo "<span class='form-success'>Success to submit a rating!</span>";
+$json=$_POST['arvostelu'];
+$arvostelu=json_decode($json, false);
 
-   }
-}
-else{
-    echo "There was an error!";
+$nickanme=$nickname->nickname;
+$kaupunki=$kaupunki->kaupunki;
+$atextarea=$atextarea->atextarea;
+$rating=$rating->rating;
+
+// Tietokantaan yhteys
+$yhteys = mysqli_connect("localhost","trtkp20a3","trtkp20a3passwd");
+
+// Tarkistetaan tietokannan yhteys
+if (!$yhteys) {
+    die("Yhteyden muodostaminen epäonnistui: " .mysql_connect_error());
+    return;
 }
 
+// Tietokannan valitseminen
+$tietokanta=mysqli_select_db($yhteys, "trtkp20a3");
+
+if(!$tietokanta) {
+    die("Tietokannan valinta epäonnistui: " .mysql_connect_error());
+    return;
+}
+
+// Tarkistetaan onko arvot tyhjiä
+if (empty($nickname) || empty($kaupunki) || empty($atextarea) || empty($rating)) {
+   print "Täytä kaikki tiedot.";
+   return;
+}
+
+
+$sql="insert into parketti_arvostelu(nickname, kaupunki, atextarea, rating) values(?, ?, ?, ?)";
+$stmt=mysqli_prepare($yhteys, $sql);
+
+mysqli_stmt_bind_param($stmt, 'ssss', $kala->laji, $kala->paino, $kala->pituus);
+mysqli_stmt_execute($stmt);
+    
+// Suljetaan statement
+mysqli_stmt_close($stmt);
+// Suljetaan SQL-yhteys
+mysqli_close($yhteys);
+print "Tiedot on tallennettu tietokantaan.";
+print $message;
+
+exit;
+    
 ?>
-
-<script>
-    // Tarkistetaan data
-
-    // Poistaa input-error vaikutuksen teksikentästä, otin arvostelu-kaupunki pois
-    $("#arvostelu-nickname, arvostelu-textarea, arvostelu-rating").removeClass("input-error");
-
-    // JavaSCript variable on yhtä suuri kuin PHP variable
-    var errorEmpty = "<?php echo $errorEmpty; ?>";
-
-    // Näytetään sivulle, jos on erroria
-    if (errorEmpty == true) {
-        // class input-error css on new.css
-        // otin arvostelu-kaupunki pois
-        $("#arvostelu-nickname, arvostelu-textarea, arvostelu-rating").addClass("input-error");
-    }
-    if (errorEmpty == false) { // otin arvostelu-kaupunki pois
-        $("#arvostelu-nickname, arvostelu-textarea, arvostelu-rating").addClass("input-error").val("");
-    }
-
-
-</script>
