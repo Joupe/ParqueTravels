0f7e3063281570cdee5c6bb21c2ecde9f02a1fa2
Joupe <75019698+Joupe@users.noreply.github.com>
2021-02-17 15:19:51 +0200

    tallennus kuntoon viä

--- tallennus.php
+++ tallennus.php
@@ -3,14 +3,14 @@
 $kohdemaa=$_POST["country"];
 $kohdekaupunki=$_POST[$kohdemaa];
 $matkan_pituus=$_POST["customRadio"];
-//$hinta=$_POST["kohdemaa"];
 $etunimi=$_POST["inputFName"];
 $sukunimi=$_POST["inputLName"];
 $osoite=$_POST["inputAddress"];
 $kaupunki=$_POST["inputCity"];
 $postinumero=$_POST["inputZip"];
 $email=$_POST["inputEmail4"];
-print("$kohdemaa, $kohdekaupunki, $matkan_pituus, $etunimi, $sukunimi, $osoite, $kaupunki, $postinumero, $email");
+
+
 
 $yhteys=mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
  if (!$yhteys){
@@ -22,24 +22,26 @@ $yhteys=mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
      die("Choosing the database failed: " . mysqli_connect_error());
  }
 
-$kohdemaa=$_POST["country"];
-$kohdekaupunki=$_POST[$kohdemaa];
-$matkan_pituus=$_POST["customRadio"];
-//$hinta=$_POST["kohdemaa"];
-$etunimi=$_POST["inputFName"];
-$sukunimi=$_POST["inputLName"];
-$osoite=$_POST["inputAddress"];
-$kaupunki=$_POST["inputCity"];
-$postinumero=$_POST["inputZip"];
-$email=$_POST["inputEmail4"];
+$tulos=mysqli_query($yhteys, "select * from parketti_hinnat where pituus = '".$matkan_pituus."' and kaupunki = '".$kohdekaupunki."'");
 
-if(isset($_POST["submitf"])){
+while ($rivi=mysqli_fetch_object($tulos)) {
+    $hinta=$rivi->hinta;
+}
+
+print("$kohdemaa, $kohdekaupunki, $matkan_pituus, $hinta, $etunimi, $sukunimi, $osoite, $kaupunki, $postinumero, $email");
+
+if(isset($_POST["submitf"])){ 
+
     $sql = "INSERT INTO parketti_varaukset(kohdemaa, kohdekaupunki, matkan_pituus, hinta, etunimi, sukunimi, osoite, kaupunki, postinumero, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($yhteys, $sql);
     mysqli_stmt_bind_param($stmt, 'ssssssssss', $kohdemaa, $kohdekaupunki, $matkan_pituus, $hinta, $etunimi, $sukunimi, $osoite, $kaupunki, $postinumero, $email);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
-    mysqli_close($yhteys);
+  
 }
+
+  header("Location:kiitos.html");
+  mysqli_close($yhteys);
+
 exit;
 ?>
